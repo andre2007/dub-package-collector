2dd0594eebe0be1faefe3f6cae3cd090adef4da2
Andre Pany <andre@s-e-a-p.de>
2020-06-26 21:36:17 +0200

    Initial commit

---
+++ source/dub.d
@@ -0,0 +1,43 @@
+import std;
+
+JSONValue getDubDescribeJSON(string root, string args)
+{
+	auto dubOutput = executeShell(`dub describe ` ~ args, null, Config.none, size_t.max, root);
+	enforce(dubOutput.status == 0, dubOutput.output);
+	return parseJSON(dubOutput.output);
+}
+
+struct DubProject
+{
+	PackageAndPath rootPackage;
+	PackageAndPath[] linkedDependencies;
+}
+
+struct PackageAndPath
+{
+	string name;
+	string version_;
+	string path;
+}
+
+string mainPackageName(string packageName)
+{
+	return packageName.split(":")[0];
+}
+
+DubProject toDubProject(JSONValue dubDescribe)
+{
+	string rootPackage = dubDescribe["rootPackage"].str;
+	string[] linkDependencies = dubDescribe["targets"].array.filter!(
+			js => js["rootPackage"].str == rootPackage).array[0]
+		.object["linkDependencies"].array.map!(js => js.str).array;
+
+	JSONValue[string] packMap = () {
+		JSONValue[string] result;
+		dubDescribe["packages"].array.each!(js => result[js["name"].str] = js);
+		return result;
+	}();
+	return DubProject(PackageAndPath(rootPackage, packMap[rootPackage].object["version"].str,
+			packMap[rootPackage].object["path"].str), linkDependencies.map!(name => PackageAndPath(name,
+			packMap[name].object["version"].str, packMap[name].object["path"].str)).array);
+}
